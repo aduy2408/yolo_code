8c87ad1f609c139d5b51732685727fc3f680a0d6
Duy <lengocduy169@gmail.com>
2026-09-02 14:46:16 +0700

    Add parser rules for project block modules

--- project_ultralytics/parser.py
+++ project_ultralytics/parser.py
@@ -37,6 +37,16 @@ def _project_parse_model(upstream_tasks: ModuleType):
             c1, c2 = ch[f], args[0]
             c2 = make_divisible(min(c2, max_channels) * width, 8)
             args = [c1, c2, *args[1:]]
+        elif m in {C2fCBAM, C3CBAM, C2f_PConv, C2fNAT}:
+            c1, c2 = ch[f], args[0]
+            c2 = make_divisible(min(c2, max_channels) * width, 8)
+            args = [c1, c2, *args[1:]]
+            args.insert(2, n)
+            n = 1
+        elif m is NATBlock:
+            c1, c2 = ch[f], args[0]
+            c2 = make_divisible(min(c2, max_channels) * width, 8)
+            args = [c1, c2, *args[1:]]
 """
     if marker not in source:
         raise RuntimeError("Unsupported upstream parse_model layout: WeightedAdd insertion point not found")
